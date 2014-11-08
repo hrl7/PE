36070d41fbe875ef95e4c53bea24874fd91f919b
Kentaro Teramoto <hrlclb@gmail.com>
2014-11-08 18:52:21 +0900

    Initial Commit

---
+++ openframeworks/src/testApp.cpp
@@ -0,0 +1,189 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+		
+	ofEnableAlphaBlending(); //透明度(アルファチャンネル)を有効にする
+	ofSetCircleResolution(64); //円の解像度を設定
+	ofSetFrameRate(120); //フレームレートの設定
+
+
+    //指定したポートで接続
+    receiver.setup( PORT_receive );
+	
+	//ofBackground(0, 0, 0);
+
+	//指定したIPアドレスとポート番号でサーバーに接続
+    sender.setup( HOST, PORT_send1 );
+	//sender2.setup( HOST, PORT_send2 );
+
+	 // シリアル通信開始
+    serial.setup("COM4",9600);
+
+	//ofBackground(200,200, 200); //背景色の設定
+     
+	//変数初期化
+	p1_x=0;
+	p1_z=0;
+	pos_z= 0;
+	pos_x= 0;
+	serial1=0;
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+	
+	
+
+	
+	
+
+	 //メッセージの受け取り
+	ofxOscMessage m;
+
+    //現在順番待ちのOSCメッセージがあるか確認
+    while( receiver.hasWaitingMessages() )
+    {
+        //次のメッセージを取得
+        
+        receiver.getNextMessage( &m );
+
+		if ( m.getAddress() == "/p1_x" ){
+			 p1_x = m.getArgAsFloat( 0 );
+
+			 if(p1_x>0){
+			// cout<<"0以上"<<endl;
+			 
+			 }
+            // cout<<p1_x<<"値"<<endl;
+        }
+
+		else if ( m.getAddress() == "/p1_z" ){
+			
+	
+			p1_z = m.getArgAsFloat( 0 );
+			
+			if(p1_z>2){
+				cout<<"2m以上"<<endl;
+				pos_z=2;
+				int numSent = serial.writeByte(1);
+			 }
+
+			else{
+				pos_z=0;
+				cout<<"2m以下"<<endl;
+				int numSent = serial.writeByte(0);
+			}
+
+         
+        }
+
+	}
+
+	
+        //OSCメッセージをそのままコンソールに出力
+      //dumpOSC(m);
+
+///////////////////////////////////////////////////////////////////////////
+
+	  //メッセージの送信
+
+	
+    //OSCメッセージの準備
+   // ofxOscMessage m;
+    //OSCアドレスの指定
+    m.setAddress( "/motor_v1" );
+    m.addFloatArg( pos_z );
+ 
+    //メッセージを送信
+    sender.sendMessage( m );
+
+	 //OSCアドレスの指定
+    m.setAddress( "/motor_v2" );
+    m.addFloatArg( pos_z );
+ 
+    //メッセージを送信
+    sender.sendMessage( m );
+
+
+  //noteoff
+	/*
+	//OSCアドレスの指定
+    m.setAddress( "/midi/noteoff" );
+    //OSC引数として、現在のマウスの座標(x, y)を送信
+    m.addIntArg( 1 );
+    m.addIntArg( x/10 );
+	m.addIntArg( y/10 );
+    //メッセージを送信
+    sender.sendMessage( m );
+	*/
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+}
+//OSCメッセージをコンソールに出力する関数
+void testApp::dumpOSC(ofxOscMessage m) {
+    string msg_string;
+    msg_string = m.getAddress();
+    for (int i=0; i<m.getNumArgs(); i++ ) {
+        msg_string += " ";
+        if(m.getArgType(i) == OFXOSC_TYPE_INT32)
+            msg_string += ofToString( m.getArgAsInt32(i));
+        else if(m.getArgType(i) == OFXOSC_TYPE_FLOAT)
+            msg_string += ofToString( m.getArgAsFloat(i));
+        else if(m.getArgType(i) == OFXOSC_TYPE_STRING)
+            msg_string += m.getArgAsString(i);
+    }
+    cout << msg_string << endl;    
+}
+//--------------------------------------------------------------
+void testApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ openframeworks/src/testApp.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofEvents.h"
+ 
+#define HOST "localhost" //送信先ホストのIPを設定
+//ポート番号を設定
+#define PORT_receive 7000
+#define PORT_send1 5001 //送信先のポート番号を設定
+#define PORT_send22 8000 //送信先のポート番号を設定 
+
+
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void dumpOSC(ofxOscMessage m); //OSCメッセージを出力
+
+//////////////////////////////////変数宣言//////////////
+
+		float p1_x;
+		float p1_z;
+
+		float pos_z;
+		float pos_x;
+
+		
+	unsigned char serial1;
+
+
+
+private:
+
+	 //OSCメッセージを受信するインスタンス
+    ofxOscReceiver    receiver;
+	 //OSCメッセージの送信者
+    ofxOscSender sender;
+	//ofxOscSender sender2;
+
+	//シリアル通信インスタンス
+	ofSerial serial;
+};
+
